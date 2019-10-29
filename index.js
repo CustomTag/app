747c7b6aea8163bd2807d180fc7c8e836e24b76a
Glitch (db-tr) <none>
2019-10-29 16:21:42 +0000

    👨‍👨‍👦🚃 Checkpoint ./index.js:5618914/314 ./html/hakkımızda.ejs:5618914/52

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ client.ayarlar = {
   "renk": "RANDOM" //embedların rengini burdan alıo can sıkıntısdna yapılmış bişe falan fln
 };
 
-client.yetkililer = ["430011871555223553"] //tüm yetkililerin ıdleri gelcek array
-client.webyetkililer = ["430011871555223553"] //web yetkililerin ıdleri gelcek array
-client.sunucuyetkililer = ["430011871555223553"] //sunucu yetkililerin ıdleri gelcek array
+client.yetkililer = ["617729641620176901"] //tüm yetkililerin ıdleri gelcek array
+client.webyetkililer = ["617729641620176901"] //web yetkililerin ıdleri gelcek array
+client.sunucuyetkililer = ["617729641620176901"] //sunucu yetkililerin ıdleri gelcek array
 
 //["id", "id2"]
 
