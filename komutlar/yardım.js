3fda85dbb6c5f9ffd525fede6a10f37ea174e87e
Glitch (db-tr) <none>
2020-05-27 18:12:49 +0000

    👳✍️ Checkpoint ./komutlar/yardım.js:13445801/91 ./html/ek/footer.ejs:13445801/27

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,11 +5,11 @@ exports.run = async (client, message, args) => {
   var embed = new Discord.RichEmbed()
   .setColor('RANDOM')
   .setAuthor("Discord Bots", client.user.avatarURL)
-  .addField("What is the purpose of the system? What does the system do? "," You can see the required information [by clicking here](https://discords-bot-list.glitch.me/hakkimizda).")
-	.addField("How do I add a bot? "," [Click here](https://discords-bot-list.glitch.me/botekle) You can teleport to the place where you can add a bot.")
-  .addField("Where can I see all the bots in the system? "," [Click here](https://discords-bot-list.glitch.me/botlar) You can see.")
-  .addField("What does the certificate do? How to get it? "," [Click here](https://discords-bot-list.glitch.me/sertifika) You can see.")
-  .addField("What can be done in the user panel? "," In the user panel, you can edit the profile / application of your bots in the system and delete your bots from the system. \ n [Click here to go to the user panel.](https://discords-bot-list.glitch.me/panel)")
+  .addField("What is the purpose of the system? What does the system do? "," You can see the required information [by clicking here](https://www.discords-bot-list.cf/hakkimizda).")
+	.addField("How do I add a bot? "," [Click here](https://www.discords-bot-list.cf/botekle) You can teleport to the place where you can add a bot.")
+  .addField("Where can I see all the bots in the system? "," [Click here](https://www.discords-bot-list.cf/botlar) You can see.")
+  .addField("What does the certificate do? How to get it? "," [Click here](https://www.discords-bot-list.cf/sertifika) You can see.")
+  .addField("What can be done in the user panel? "," In the user panel, you can edit the profile / application of your bots in the system and delete your bots from the system. \ n [Click here to go to the user panel.](https://www.discords-bot-list.cf/panel)")
   message.channel.send({embed: embed})
   
 };
