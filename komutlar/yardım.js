392b099722f7419b5de713ceb9a92d11d87c6c1d
Glitch (db-tr) <none>
2020-06-27 21:07:56 +0000

    🚴🚐 Checkpoint ./komutlar/yardım.js:13445801/1202

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -4,12 +4,12 @@ exports.run = async (client, message, args) => {
 	//kendi URL'lerini eklersn :D
   var embed = new Discord.RichEmbed()
   .setColor('RANDOM')
-  .setAuthor("Discord Bot List", client.user.avatarURL)
-  .addField("What is the purpose of the system? What does the system do? "," You can see the required information [by clicking here](https://www.discords-bot-list.cf/hakkimizda).")
-	.addField("How do I add a bot? "," [Click here](https://www.discords-bot-list.cf/botekle) You can teleport to the place where you can add a bot.")
-  .addField("Where can I see all the bots in the system? "," [Click here](https://www.discords-bot-list.cf/botlar) You can see.")
-  .addField("What does the certificate do? How to get it? "," [Click here](https://www.discords-bot-list.cf/sertifika) You can see.")
-  .addField("What can be done in the user panel? "," In the user panel, you can edit the profile / application of your bots in the system and delete your bots from the system. \ n [Click here to go to the user panel.](https://www.discords-bot-list.cf/panel)")
+  .setAuthor("Discord bot list Page", client.user.avatarURL)
+  .addField("Website Creator","Discords bot list, Created by CustomTag#0001")
+	.addField("Add a bot?","[Click here](https://www.discords-bot-list.cf/botekle)")
+  .addField("Hosted by","[Glitch](https://glitch.com)")
+  .addField("Website","[Click here](https://www.discords-bot-list.cf)")
+  .addField("Search Bot","?search (botid)")
   message.channel.send({embed: embed})
   
 };
@@ -19,11 +19,11 @@ exports.conf = {
 	guildOnly: true,
 	aliases: ['y', 'help', 'h', 'bilgi', 'info'],
 	permLevel: 0,
-	kategori: 'testing-1'
+	kategori: 'general'
 }
 
 exports.help = {
 	name: 'help',
-	description: 'Shows information about the system.',
+	description: 'Shows information about the system!',
 	usage: 'help'
 }
