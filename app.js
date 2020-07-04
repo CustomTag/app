1b2ccfdaa133df4ed1bdf15f0d073a084a1511d2
Glitch (discord4bots) <none>
2020-07-04 14:18:59 +0000

    🙍🍨 Checkpoint ./app.js:13445801/196

--- app.js
+++ app.js
@@ -392,12 +392,9 @@ db.set(`bots.${ID}.support`, ayar['botsupport'])
 res.redirect("/user/"+req.params.userID+"/panel");
 
 client.channels.get(client.settings.kayıt).send(`\`${req.user.username}#${req.user.discriminator}\` Has Edited: \`${sistem.username}#${sistem.discriminator}\` https://discord4bots.glitch.me/bot/${db.fetch(`bots.${ID}.id`)}`)
-
-if (client.users.has(req.user.id) === true) {
-client.users.get(req.user.id).send(`\`${sistem.username}#${sistem.discriminator}\` Your bot's profile / application has been successfully edited!`)
 }
 
-}})
+})
 }})
 
 });
