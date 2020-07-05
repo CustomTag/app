84886712b8627f026305cf33f2e59aefea4fa19f
Glitch (discord4bots) <none>
2020-07-05 21:51:41 +0000

    🚏🌇 Checkpoint ./commands/help.js:16402558/84 ./commands/cmds.js:16402558/1430

---
+++ commands/cmds.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const db = require('quick.db')
+
+exports.run = async (client, message, args, msg) => {
+  let prefix = await db.fetch(`${client.settings.prefix}`)
+  var embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setAuthor("Discord4Bots - Commands", client.user.avatarURL)
+  
+  message.channel.send({embed: embed})
+  
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: true,
+	aliases: ['cmds', 'commands', 'command'],
+	permLevel: 0,
+	kategori: 'general'
+}
+
+exports.help = {
+	name: 'cmds',
+	description: 'All commands!',
+	usage: 'cmds'
+}
