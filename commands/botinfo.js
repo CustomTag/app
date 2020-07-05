acc65ba797c45383ffc4e504b7b11cef87ba949e
Glitch (discord4bots) <none>
2020-07-05 18:48:09 +0000

    🛢🦃 Checkpoint ./commands/botinfo.js:13445801/652 ./commands/eval.js:13445801/8

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -3,57 +3,57 @@ const db = require('quick.db')
 const request = require('request')
 
 exports.run = async (client, msg, args) => {
-    let prefix = await db.fetch(`${msg.guild.id}.prefix`) || client.ayarlar.prefix
+    let prefix = await db.fetch(`${msg.guild.id}.prefix`) || client.settings.prefix
     if(!args[0]) {
-      return msg.channel.send(new Discord.RichEmbed().setDescription('Lütfen Bir Bot ID\'i Giriniz!').setColor("RANDOM"))
+      return msg.channel.send(new Discord.RichEmbed().setDescription('Please enter a bot ID!').setColor("RANDOM"))
     }
-    request(`https://turkbotlar.glitch.me/api/botlar/${args[0]}`, function (error, response, body) {
-    if (error) return msg.channel.send('Hata:', error);
+    request(`https://discord4bots.glitch.me/api/bots/${args[0]}`, function (error, response, body) {
+    if (error) return msg.channel.send('Error:', error);
     else if (!error) {
-      var a = JSON.parse(body).isim
+      var a = JSON.parse(body).name
       var b = JSON.parse(body).id
       var c = `${JSON.parse(body).avatar}`
       var d = JSON.parse(body).prefix
-      var e = JSON.parse(body).kütüphane
-      var f = `${JSON.parse(body).sahip} (${JSON.parse(body).sahipid})`
-      var g = JSON.parse(body).kisa_aciklama
-      var h = JSON.parse(body).etiketler
-      if(JSON.parse(body).destek_sunucusu === 'Belirtilmemiş') {
-        var i = 'Belirtilmemiş'
+      var e = JSON.parse(body).library
+      var f = `${JSON.parse(body).owner} (${JSON.parse(body).ownerid})`
+      var g = JSON.parse(body).ShortDesc
+      var h = JSON.parse(body).labels
+      if(JSON.parse(body).support === 'Unspecified') {
+        var i = 'Unspecified'
       } else {
-        var i = `[${a} Destek Sunucusu](${JSON.parse(body).destek_sunucusu})`
+        var i = `[${a} Support Server](${JSON.parse(body).support})`
       }
-      if(JSON.parse(body).web_sitesi === 'Belirtilmemiş') {
-        var j = 'Belirtilmemiş'
+      if(JSON.parse(body).site === 'Unspecified') {
+        var j = 'Unspecified'
       } else {
-      var j = JSON.parse(body).web_sitesi
+      var j = JSON.parse(body).site
       }
-      if(JSON.parse(body).github === 'Belirtilmemiş')  {
-        var k = 'Belirtilmemiş'
+      if(JSON.parse(body).github === 'Unspecified')  {
+        var k = 'Unspecified'
       } else {
         var k = `[Github](${JSON.parse(body).github})`
       }
-      var l = JSON.parse(body).sertifika
-      var m = JSON.parse(body).durum
-      var n = JSON.parse(body).oy_sayisi
+      var l = JSON.parse(body).certificate
+      var m = JSON.parse(body).status
+      var n = JSON.parse(body).votes
     }
       
-      request(`https://fiboxbotlist.glitch.me/api/tumbotlar`, function (errorr, responsee, bodyy) {
-    if (errorr) return msg.channel.send('Hata:', errorr);
+      request(`https://discord4bots.glitch.me/api/allbots`, function (errorr, responsee, bodyy) {
+    if (errorr) return msg.channel.send('Error:', errorr);
     else if (!errorr) {
-    if (bodyy.includes(args[0])=== false) return msg.reply("Bu ID'de Bir Bot Sistemde Bulunmamaktadır!")
+    if (bodyy.includes(args[0])=== false) return msg.reply("There is no bot in this ID system!")
     }
        })
       
     const embed = new Discord.RichEmbed()
     .setColor("RANDOM")
     .setThumbnail(c)
-    .setTitle(`Discord4Bots - `)
-    .setDescription(`${a} (${b}) [${n} oy]`, c)
+    .setTitle(`Discord4Bots - BotSearch`)
+    .setDescription(`${a} (${b}) [${n} Votes]`, c)
     .addField('Prefix', d)
     .addField('Owner', f)
     .addField('Short Description', g)
-    .addField('labelslabels', h)
+    .addField('labels', h)
     .addField('Certificate', l)
     .addField('Status', m)
     .addField("Website", j)
@@ -75,5 +75,5 @@ exports.conf = {
 exports.help = {
   name: 'botinfo',
   description: 'Lets You Search For Bots In The Discord4Bots System.',
-  usage: 'botinfo [ID]'
+  usage: 'botinfo [id]'
 };
