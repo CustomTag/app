2a0fdbdf8457f9c518863570cbb9f0ad9544676a
Glitch (discord4bots) <none>
2020-07-22 13:08:07 +0000

    Rewound to commit 25b714f0b3b318ca063309a001822f1704aa7ca0 + 0 edits

--- app.js
+++ app.js
@@ -217,7 +217,7 @@ db.set(`kbots.${req.user.id}.${ID}`, db.fetch(`bots.${ID}`))
 
 res.redirect("/user/"+req.params.userID+"/panel");
 
-client.channels.get(client.settings.kayıt).send(`\`${req.user.username}#${req.user.discriminator}\` just added \`${sistem.username}#${sistem.discriminator}\` https://discord4bots.glitch.me/bot/${db.fetch(`bots.${ID}.id`)} <@714499240876965918>`)
+client.channels.get(client.settings.kayıt).send(`\`${req.user.username}#${req.user.discriminator}\` just added \`${sistem.username}#${sistem.discriminator}\` https://discord4bots.glitch.me/bot/${db.fetch(`bots.${ID}.id`)}`)
 
 if (client.users.has(db.fetch(`bots.${ID}.ownerid`)) === true) {
 client.users.get(db.fetch(`bots.${ID}.ownerid`)).send(`\`${db.fetch(`bots.${ID}.name`)}\` Has been added to pending https://discord4bots.glitch.me/bot/${db.fetch(`bots.${ID}.id`)}`)
