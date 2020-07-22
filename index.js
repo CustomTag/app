fe1445cd7b1aeae7f583f05c1d6831cad5c2e8dc
Glitch (discord4bots) <none>
2020-07-22 16:47:19 +0000

    👐📒 Checkpoint ./app.js:13445801/22 ./html/css/genel.css:13445801/80 ./html/bot.ejs:13445801/14

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.profile = profile
 
 client.settings = {
   "prefix": "c!", //prefix
-  "oauthSecret": "hCeVH3zaAC_7GYf_tsMac8hq9qGEiIaI", // bot secreti
+  "oauthSecret": "EAk0RrL8OxRKA5Vv9f0qkkQ6dlX7xB6w", // bot secreti
 	"callbackURL": "https://discord4bots.glitch.me/callback", // change the urls of my site with "/ callback"!
 	"kayıt": "714504216370085929", // approved, rejected, you have applied, you will write the ID of the channel where the recordings will go
   "color": "RANDOM" // get the color of the embeds from here, something like that
