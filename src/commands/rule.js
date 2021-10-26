1cb609fc334134c57141c5de121ec7e2664aff2e
Glitch (discordtown) <none>
2021-10-26 11:45:57 +0000

    😂⛺️ Checkpoint ./src/commands/rule.js:6036931/242 ./src/views/admin/index.ejs:16547955/49 ./src/views/parts/foot.ejs:14194749/655 ./src/views/parts/meta.ejs:14194749/205

--- src/commands/rule.js
+++ src/commands/rule.js
@@ -1,5 +1,19 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports.run = (client, message, args) => {
+  const embed = new MessageEmbed()
+  .setTItle('DIscord Town | Rules')
   message.channel.send('TESTING!');
 }
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+};
+
+exports.help = {
+  name: "rule",
+  description: "RULES!",
+  usage: ""
+};
